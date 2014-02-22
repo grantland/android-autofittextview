cc5cbe085b8cc54c280a964558b94842e63f6e26
Grantland Chew <grantlandchew@gmail.com>
2014-02-22 00:10:49 -0800

    Clean up logic
    * Better refit logic
    * Default minTextSize to use sp

--- library/src/main/java/me/grantland/widget/AutofitTextView.java
+++ library/src/main/java/me/grantland/widget/AutofitTextView.java
@@ -3,6 +3,7 @@ package me.grantland.widget;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Paint;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -19,7 +20,7 @@ public class AutofitTextView extends TextView {
     private static final boolean SPEW = false;
 
     // Minimum size of the text in pixels
-    private static final int DEFAULT_MIN_TEXT_SIZE = 8; //px
+    private static final int DEFAULT_MIN_TEXT_SIZE = 8; //sp
     // How precise we want to be when reaching the target textWidth size
     private static final float PRECISION = 0.5f;
 
@@ -31,37 +32,60 @@ public class AutofitTextView extends TextView {
 
     public AutofitTextView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public AutofitTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
-    private void init() {
-        mMinTextSize = DEFAULT_MIN_TEXT_SIZE;
-        mMaxTextSize = getTextSize();
+    private void init(Context context) {
+        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+        mMinTextSize = scaledDensity * DEFAULT_MIN_TEXT_SIZE;
         mPrecision = PRECISION;
         mPaint = new Paint();
+        setRawTextSize(super.getTextSize());
+        //TODO allow multiple lines and ellipsize settings
+        setMaxLines(1);
+        setEllipsize(TextUtils.TruncateAt.END);
     }
 
     // Getters and Setters
+
+    @Override
+    public float getTextSize() {
+        return mMaxTextSize;
+    }
+
+    @Override
+    public void setTextSize(int unit, float size) {
+        Context context = getContext();
+        Resources r = Resources.getSystem();
+
+        if (context != null) {
+            r = context.getResources();
+        }
+
+        setRawTextSize(TypedValue.applyDimension(unit, size, r.getDisplayMetrics()));
+    }
+
+    private void setRawTextSize(float size) {
+        if (size != mMaxTextSize) {
+            mMaxTextSize = size;
+            refitText();
+        }
+    }
 
     public float getMinTextSize() {
         return mMinTextSize;
     }
 
     public void setMinTextSize(int minTextSize) {
-        mMinTextSize = minTextSize;
-    }
-
-    public float getMaxTextSize() {
-        return mMaxTextSize;
-    }
-
-    public void setMaxTextSize(int maxTextSize) {
-        mMaxTextSize = maxTextSize;
+        if (minTextSize != mMinTextSize) {
+            mMinTextSize = minTextSize;
+            refitText();
+        }
     }
 
     public float getPrecision() {
@@ -69,21 +93,25 @@ public class AutofitTextView extends TextView {
     }
 
     public void setPrecision(float precision) {
-        mPrecision = precision;
+        if (precision != mPrecision) {
+            mPrecision = precision;
+            refitText();
+        }
     }
 
     /**
      * Re size the font so the specified text fits in the text box
      * assuming the text box is the specified width.
      */
-    private void refitText(String text, int width) {
-        if (width > 0) {
+    private void refitText() {
+        String text = getText().toString();
+        int targetWidth = getWidth() - getPaddingLeft() - getPaddingRight();
+        if (targetWidth > 0) {
             Context context = getContext();
             Resources r = Resources.getSystem();
 
-            int targetWidth = width - getPaddingLeft() - getPaddingRight();
-            float newTextSize = mMaxTextSize;
-            float high = mMaxTextSize;
+            float size = mMaxTextSize;
+            float high = size;
             float low = 0;
 
             if (context != null) {
@@ -91,38 +119,38 @@ public class AutofitTextView extends TextView {
             }
 
             mPaint.set(getPaint());
-            mPaint.setTextSize(newTextSize);
+            mPaint.setTextSize(size);
 
             if (mPaint.measureText(text) > targetWidth) {
-                newTextSize = getTextSize(r, text, targetWidth, low, high);
+                size = getTextSize(r, text, targetWidth, low, high, mPrecision, mPaint);
 
-                if (newTextSize < mMinTextSize) {
-                    newTextSize = mMinTextSize;
+                if (size < mMinTextSize) {
+                    size = mMinTextSize;
                 }
             }
 
-            setTextSize(TypedValue.COMPLEX_UNIT_PX, newTextSize);
-
+            super.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
         }
     }
 
     // Recursive binary search to find the best size for the text
-    private float getTextSize(Resources resources, String text, float targetWidth, float low, float high) {
+    private static float getTextSize(Resources resources, String text, float targetWidth,
+                                     float low, float high, float precision, Paint paint) {
         float mid = (low + high) / 2.0f;
 
-        mPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, mid, resources.getDisplayMetrics()));
-        float textWidth = mPaint.measureText(text);
+        paint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, mid, resources.getDisplayMetrics()));
+        float textWidth = paint.measureText(text);
 
         if (SPEW) Log.d(TAG, "low=" + low + " high=" + high + " mid=" + mid + " target=" + targetWidth + " width=" + textWidth);
 
-        if ((high - low) < mPrecision) {
+        if ((high - low) < precision) {
             return low;
         }
         else if (textWidth > targetWidth) {
-            return getTextSize(resources, text, targetWidth, low, mid);
+            return getTextSize(resources, text, targetWidth, low, mid, precision, paint);
         }
         else if (textWidth < targetWidth) {
-            return getTextSize(resources, text, targetWidth, mid, high);
+            return getTextSize(resources, text, targetWidth, mid, high, precision, paint);
         }
         else {
             return mid;
@@ -130,24 +158,17 @@ public class AutofitTextView extends TextView {
     }
 
     @Override
-    protected void onTextChanged(final CharSequence text, final int start, final int lengthBefore, final int lengthAfter) {
+    protected void onTextChanged(final CharSequence text, final int start,
+                                 final int lengthBefore, final int lengthAfter) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter);
-        refitText(text.toString(), this.getWidth());
+        refitText();
     }
 
     @Override
     protected void onSizeChanged (int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         if (w != oldw) {
-            refitText(getText().toString(), w);
+            refitText();
         }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-    {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
-        refitText(getText().toString(), parentWidth);
     }
 }
