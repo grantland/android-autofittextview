436eafb4049a95687b08d1c22676ba24a57f534e
Grantland Chew <grantlandchew@gmail.com>
2014-04-07 23:21:16 -0700

    Make gradle happy

--- sample/src/main/java/me/grantland/autofittextview/sample/SampleActivity.java
+++ sample/src/main/java/me/grantland/autofittextview/sample/SampleActivity.java
@@ -1,12 +1,9 @@
-package me.grantland.autofittextview;
+package me.grantland.autofittextview.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
